e4803365415f9e5ca581abc3222eebfa86541bea
homerade <mizzsvizz@gmail.com>
2018-05-03 13:40:17 -0400

    image fix

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -4,7 +4,7 @@
 		<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
 		  <div class="carousel-inner">
 		    <div class="carousel-item active">
-		      <img class="d-block w-50" src="/assets/images/handfull_rs.jpg" alt="First slide">
+		      <img class="d-block w-50" src="/assets/images/handFull_rs.jpg" alt="First slide">
 		    </div>
 		    <div class="carousel-item">
 		      <img class="d-block w-50" src="/assets/images/whiteAsperge.jpg" alt="Second slide">
